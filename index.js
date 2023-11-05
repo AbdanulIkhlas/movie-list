191c920f16b81d8cb1d0b58ec899e67b8e656afc
KLaZ <muhabdanulikhlas0983@gmail.com>
2023-11-05 23:01:29 +0700

    menambahkan data film dari api omdb

--- index.js
+++ index.js
@@ -1,41 +1,54 @@
 const search = document.querySelector("#search");
-let movie
-
+let cards;
 //! Fetch API menggunakan axios
 function onChangeSearch(searchParameter){
-    axios.get("http://www.omdbapi.com/?apikey=d684a20e&t=" + searchParameter, {
+    axios.get("http://www.omdbapi.com/?apikey=d684a20e&s=" + searchParameter, {
 
     })
     .then(function (response) {
-        if(response.data.Title){
-            movie = response.data;
-        }else{
-            movie.Title = "Tidak Ada Filmnya.."
-            movie.Poster = "uu"
-        }
+        const movies = response.data.Search;
+        console.log(movies);
+        cards = '';
+        movies.forEach(data => cards += showCards(data));
+        console.log(cards)
+        // if(response.data.Title){
+        // }else{
+        //     movie.Title = "Tidak Ada Filmnya.."
+        //     movie.Poster = "uu"
+        // }
         
     })
     .catch(function (error) {
         console.log(error);
     })
     .finally(function () {
-        const main = document.querySelector('#main');
+        const allCards = document.querySelector('.all-cards');
 
-        main.innerHTML = showCards(movie);
+        // allCards.innerHTML = showCards(movie);
+        allCards.innerHTML = cards;
     }); 
 }
 
 function showCards(data){
+    console.log(data)
+    // let countryArray = data.Country.split(', ');
+    // let country = countryArray[0];
+    // <div class="tahun"> ${data.Year} </div>
     return `
-    <div class="card">
-        <h1>${data.Title}</h1>
-        <img src="${data.Poster}"
-            title="${data.Title}">
+    <div class="container-card">
+        <div class="card">
+            <div class="info">
+                <div class="negara"> ${data.Year} </div>
+            </div>
+            <img id="gambar" src="${data.Poster}" alt="">
+            <div class=" genre"> ${data.Type}</div>
+        </div>
+        <label for="gambar" id="judul"> ${data.Title} </label>
+        <div id="tooltip"> ${data.Title}</div>
     </div>
     `
 }
-
 // ! event untuk searching
-input.addEventListener('blur', function(){
-    onChangeSearch(input.value);
+search.addEventListener('blur', function(){
+    onChangeSearch(search.value);
 })
